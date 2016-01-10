513f09827448dc90750e3f00076ac7296d6e5524
Nicky Vandevoorde <nicky.vandevoorde@gmail.com>
2016-01-10 14:37:19 +0000

    Add php lint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,43 @@
 var gulp = require('gulp');
 var phpcs = require('gulp-phpcs');
 var shell = require('gulp-shell');
+var phplint = require('phplint').lint;
+
+paths = {
+  php: ['**/*.{php,inc,module}', '!node_modules/**', '!vendor/**']
+};
 
 /**
  * Codesniffs on correct Drupal standards.
  */
 gulp.task('phpcs-drupal', function () {
-    return gulp.src(['./**/*.php', './**/*.inc', './**/*.module', '!node_modules/', '!vendor/**/*'])
-        // Codesniff on correct Drupal standards.
-        .pipe(phpcs({
-            bin: 'vendor/bin/phpcs',
-            standard: 'vendor/drupal/coder/coder_sniffer/Drupal',
-            warningSeverity: 0,
-            colors: 1
-        }))
-        // Report all problems to the console.
-        .pipe(phpcs.reporter('log'));
+  return gulp.src(paths.php)
+    // Codesniff on correct Drupal standards.
+    .pipe(phpcs({
+        bin: 'vendor/bin/phpcs',
+        standard: 'vendor/drupal/coder/coder_sniffer/Drupal',
+        warningSeverity: 0,
+        colors: 1
+    }))
+    // Report all problems to the console.
+    .pipe(phpcs.reporter('log'));
 });
- 
+
 /**
- * Cleanup all unnecessary dev dependencies again after coding.
+ * Lints php files.
+ */
+gulp.task('phplint', function (cb) {
+  phplint(paths.php,  { limit: 10 }, function (err, stdout, stderr) {
+    if (err) {
+        cb(err.message);
+    } else {
+        cb();
+    }
+  });
+});
+
+/**
+ * Cleans up all unwanted dev dependencies after coding.
  */
 gulp.task('prod', shell.task([
 	// Remove Node dev depedencies
@@ -31,12 +49,15 @@ gulp.task('prod', shell.task([
 /**
  * Watch task for code changes.
  */
-gulp.task('default', function() {
-  gulp.watch(['./**/*.php', './**/*.inc', './**/*.module', '!node_modules/', '!vendor/**/*'], ['phpcs-drupal']);
+gulp.task('default', function () {
+  gulp.watch(paths.php, ['phplint', 'phpcs-drupal']);
 });
 
-gulp.task('production', ['prod']);
-gulp.task('clean', ['prod']);
-gulp.task('cleanup', ['prod']);
-gulp.task('build', ['prod']);
-gulp.task('server', ['prod']);
+// Sets sensible aliases for tasks.
+gulp.task('watch',        ['default']);
+gulp.task('production',   ['prod']);
+gulp.task('clean',        ['prod']);
+gulp.task('cleanup',      ['prod']);
+gulp.task('build',        ['prod']);
+gulp.task('server',     ['prod']);
+
